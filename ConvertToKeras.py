fa1cb11438d81c5d20ff3bbc732f08c4f02297fa
lukelike1001 <lsn12@duke.edu>
2024-05-08 23:56:43 -0700

    compile typo fix

--- ConvertToKeras.py
+++ ConvertToKeras.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 from tensorflow import keras
 
 # Load the old model using a folder then convert it into a h5 file
-folder_model = tf.keras.models.load_model('saved_model/', compile=False)
+folder_model = tf.keras.models.load_model('saved_model/')
 folder_model.compile(optimizer="adam",
                      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                      metrics=["accuracy"])
